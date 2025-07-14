81e005bce95adca14ad862e8d29a1a61bebaf456
Aurelien Cormoreche <aurelien.cormoreche@gmail.com>
2025-07-14 18:34:30 +0200

    added possibility to plot moving averages

---
+++ utils/utils_plotting.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_file(filepath):
+    data = np.loadtxt(filepath)
+    return data[:, 1]
+
+
+def compute_moving_average(filepath,window_size):
+    data = load_file(filepath)
+
+    return np.convolve(data, np.ones(window_size)/window_size, mode= 'valid')
+
+
+
+
+
+
+if __name__ == '__main__':
+    
+    tab = [20,50,100,300,500]
+    for t in tab:
+        mv_avg = compute_moving_average('mlruns/244787145723528822/e677b4afb3e349e48481f15f21970daf/metrics/run length', t)
+        plt.plot(mv_avg)
+        plt.show()
