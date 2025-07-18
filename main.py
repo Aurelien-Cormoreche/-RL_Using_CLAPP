50c26d7d6d50b0d146881441e1414c9e0f0d8f13
Aurelien Cormoreche <aurelien.cormoreche@gmail.com>
2025-07-18 14:59:00 +0200

    fixed compile issue

--- main.py
+++ main.py
@@ -42,15 +42,17 @@ def train(opt, envs, model_path, device, models_dict):
         return
     
 
-    encoder.to(device)
-    encoder.compile(backend="aot_eager")
+    encoder = encoder.to(device)
+    encoder = encoder.half()
+    encoder = encoder.compile(backend="aot_eager")
+
     
     for param in encoder.parameters():
         param.requires_grad = False
     
     action_dim = envs.single_action_space.n
     feature_dim = feature_dim * opt.nb_stacked_frames
-    
+
 
     if opt.track_run:
         mlflow.start_run(run_name= opt.run_name)
